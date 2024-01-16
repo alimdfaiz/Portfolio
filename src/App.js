74e8c9533396a05bd8cfe5b76749152a01e2dcbb
alimdfaiz <alimdfaiz6@gmail.com>
2024-01-16 20:31:16 +0530

    portfolio react

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./style.css";
+import Navbar from "./Components/Navbar";
+import About from "./Components/About";
+import Section from "./Components/Section";
+import Footer from "./Components/Footer";
+
+
+const App = () => {
+    return(
+        <div>
+      <Navbar />
+      <About />
+      <Section />
+      <Footer />
+        </div>
+    )
+}
+
+
+
+
+
+export default App;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+const Navbar = () => {
+    return(
+        <nav>
+       <h1>Md Faiz Ali</h1>
+        <div className="nav">
+     
+     <a href="#">Home</a>
+     <a href="#">About</a>
+     <a href="#">Skills</a>
+     <a href="#">Qualification</a>
+        </div>
+        </nav>
+    )
+}
+
+export default Navbar;
